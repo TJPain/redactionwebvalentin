d5b50fdb5e65412eb4c9003b2f27ef2a28b0c597
Tom Pain <tom.pain@midnite.com>
2023-01-21 13:41:35 +0000

    Home page services carousel

--- frontend/components/home/ServicesCarousel.vue
+++ frontend/components/home/ServicesCarousel.vue
@@ -1,23 +1,48 @@
 <template>
-  <section max-w="300" w="full" m="x-auto y-8 md:y-15">
-    <h3 font="semibold" text="dark-600 2xl md:3xl center" m="b-5">
+  <section max-w="300" w="full" m="x-auto y-8 md:y-15" p="x-5 md:x-10">
+    <h3 font="semibold" text="dark-600 2xl md:3xl center" m="b-15">
       Services
     </h3>
-    <div v-for="(item, index) in services" :key="index">
-      {{ item.title }}
+    <div flex="~ col md:row" space="md:x-15">
+      <div w="full md1/3" flex="~ col auto" align="items-center" m="b-10">
+        <img src="~/static/home/redaction-web.png" w="20" h="auto" m="b-5">
+        <h4 font="semibold" text="dark-600 xl md:2xl center md:left" m="b-5">
+          Rédaction web et copywriting
+        </h4>
+        <p text="center md:left" m="b-5">
+          J'assoie votre notoriété et renforce votre expertise en rédigeant vos contenus web.
+        </p>
+        <p text="center md:left" m="b-5">
+          Qu'il s'agisse de billets de blog, de newsletters, de légendes de posts sur les réseaux sociaux, de fiches produits ou encore de livres blanc ou de contenus journalistiques. J'écris <strong>pour vous et selon vos objectifs.</strong>
+        </p>
+      </div>
+      <div w="full md1/3" flex="~ col auto" align="items-center" m="b-10">
+        <img src="~/static/home/traduction.png" w="20" h="auto" m="b-5">
+        <h4 font="semibold" text="dark-600 xl md:2xl center md:left" m="b-5">
+          Traduction
+        </h4>
+        <p text="center md:left" m="b-5">
+          Je vous aide à étendre votre présence en ligne aux secteurs francophones.
+        </p>
+        <p text="center md:left" m="b-5">
+          Je m'occupe de traduire vos textes, articles, ou sites web dans le <strong>respect de votre ligne éditoriale</strong> et en m'assurant que tout y est !
+        </p>
+      </div>
+      <div w="full md1/3" flex="~ col auto" align="items-center" m="b-10">
+        <img src="~/static/home/optimisation-seo.png" w="20" h="auto" m="b-5">
+        <h4 font="semibold" text="dark-600 xl md:2xl center md:left" m="b-5">
+          Optimisation SEO
+        </h4>
+        <p text="center md:left" m="b-5">
+          J'augmente le trafic organique sur votre site et étend votre visibilité grâce au référencement naturel SEO.
+        </p>
+        <p text="center md:left" m="b-5">
+          Que ce soit votre site web, un billet de blog ou encore votre page produits ou vos visuels, <strong>j'améliore votre positionnement dans les résultats de recherches</strong> sur internet.
+        </p>
+        <p text="center md:left" m="b-5">
+          Rédaction SEO de contenus optimisés pour les algorithmes. Recherche et ajout de mot-clés pertinents, création d'un netlinking et rédaction des balises méta.
+        </p>
+      </div>
     </div>
   </section>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      services: [
-        { title: '1' },
-        { title: '2' }
-      ]
-    }
-  }
-}
-</script>
