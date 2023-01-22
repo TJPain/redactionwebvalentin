f4e4820c6b35e15fdab373caf33a3c00c013f8b3
Tom Pain <tom.pain@midnite.com>
2023-01-22 11:42:28 +0000

    Mobile menu

---
+++ frontend/components/global/MobileMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sticky w-full bg-white z-40 inset-0 p-10" flex="~ col auto" h="screen" overflow="y-hidden">
+    <div class="overflow-y-auto" m="t-5">
+      <div flex="~ col auto" align="items-end" p="t-3">
+        <button class="ml-1 flex items-center justify-center h-10" text="dark-700 xl" font="semibold" @click="$emit('closeMobileMenu')">
+          X
+        </button>
+      </div>
+      <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 flex-col md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-medium md:border-0 md:bg-white" m="b-0">
+        <li>
+          <a href="/#qui" class="block py-2 pl-3 pr-4 text-dark-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0" @click="$emit('closeMobileMenu')">
+            Qui Suis-Je
+          </a>
+        </li>
+        <li>
+          <a href="/#bonProfil" class="block py-2 pl-3 pr-4 text-dark-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0" @click="$emit('closeMobileMenu')">
+            Avez-Vous Le Bon Profil ?
+          </a>
+        </li>
+        <li>
+          <a href="/#services" class="block py-2 pl-3 pr-4 text-dark-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0" @click="$emit('closeMobileMenu')">
+            Services
+          </a>
+        </li>
+        <li>
+          <NuxtLink to="/blog" class="block py-2 pl-3 pr-4 text-dark-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0" @click="$emit('closeMobileMenu')">
+            Blog
+          </NuxtLink>
+        </li>
+        <li>
+          <a href="/#contactez" class="block py-2 pl-3 pr-4 text-dark-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0" @click="$emit('closeMobileMenu')">
+            Contactez Moi
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div max-w="500" m="x-auto">
-    <NavBar />
+    <NavBar @toggleMobileMenu="showMobileMenu = !showMobileMenu" />
+    <transition
+      enter-active-class="transition-opacity ease-linear duration-300"
+      enter-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition-opacity ease-linear duration-300"
+      leave-class="opacity-0"
+      leave-to-class="opacity-100"
+    >
+      <MobileMenu v-show="showMobileMenu" @closeMobileMenu="showMobileMenu = false" />
+    </transition>
     <div p="t-15">
       <Nuxt />
     </div>
@@ -9,10 +19,17 @@
 
 <script>
 import NavBar from '~/components/global/NavBar.vue'
+import MobileMenu from '~/components/global/MobileMenu.vue'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    MobileMenu
+  },
+  data () {
+    return {
+      showMobileMenu: false
+    }
   }
 }
 </script>
