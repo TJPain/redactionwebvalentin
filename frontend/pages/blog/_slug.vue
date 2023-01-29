636ced3a666e627b7b3fd04922a8afd6fe2c6955
Tom Pain <tom.pain@midnite.com>
2023-01-29 10:53:28 +0000

    Use slugs instead of ids for blog pages

--- frontend/pages/blog/_slug.vue
+++ frontend/pages/blog/_slug.vue
@@ -9,7 +9,7 @@
     />
     <div w="full" h="155" p="x-5 md:x-10">
       <div
-        :style="{ backgroundImage: `linear-gradient(0deg, rgba(17,17,17,0.60) 0%, rgba(17,17,17,0.60) 100%), url(${api_url + blog.data.attributes.image.data.attributes.url})`}"
+        :style="{ backgroundImage: `linear-gradient(0deg, rgba(17,17,17,0.60) 0%, rgba(17,17,17,0.60) 100%), url(${api_url + blogs.data[0].attributes.image.data.attributes.url})`}"
         bg="cover"
         w="full"
         h="155"
@@ -19,18 +19,18 @@
         p="8 md:15"
       >
         <h1 font="bold" text="white center 5xl md:6xl">
-          {{ blog.data.attributes.title }}
+          {{ blogs.data[0].attributes.title }}
         </h1>
         <p text="white center 2xl md:3xl" font="semibold">
-          {{ blog.data.attributes.subtitle }}
+          {{ blogs.data[0].attributes.subtitle }}
         </p>
       </div>
     </div>
     <div p="t-15 b-5">
       <div class="uk-container uk-container-small">
-        <div v-if="blog.data.attributes.content" id="editor" m="b-8" v-html="$md.render(blog.data.attributes.content)" />
-        <p v-if="blog.data.publishedAt" m="b-8">
-          {{ blog.data.attributes.publishedAt }}
+        <div v-if="blogs.data[0].attributes.content" id="editor" m="b-8" v-html="$md.render(blogs.data[0].attributes.content)" />
+        <p v-if="blogs.data[0].publishedAt" m="b-8">
+          {{ blogs.data[0].attributes.publishedAt }}
         </p>
       </div>
     </div>
@@ -52,7 +52,7 @@ export default {
   data () {
     return {
       api_url: process.env.strapiBaseUri,
-      blog: {
+      blogs: {
         data: []
       },
       categories: {
@@ -61,14 +61,14 @@ export default {
     }
   },
   computed: {
-    getBlog () { return this.blog.data.attributes ? this.blog.data : null }
+    getBlog () { return this.blogs.data[0].attributes ? this.blogs.data[0] : null }
   },
   apollo: {
-    blog: {
+    blogs: {
       prefetch: true,
       query: blogQuery,
       variables () {
-        return { id: parseInt(this.$route.params.id) }
+        return { slug: this.$route.params.slug }
       }
     },
     categories: {
